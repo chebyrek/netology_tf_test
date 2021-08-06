9a1df1eebc9772846fcfae33ddbd45043aeae2ce
Nicolay Chepurnenko <nchepurnenko@mail.ru>
2021-08-06 06:30:32 +0000

    2

--- main.tf
+++ main.tf
@@ -2,21 +2,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-    most_recent = true
-
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-amd64-server-*"]
-    }
-
-    owners = ["099720109477"] # Canonical
-}
-
-
 resource "aws_instance" "test2" {
-    for_each = toset(["1","2"])
-    ami           = "${data.aws_ami.ubuntu.id}"
+    ami           = "ami-0a12b6f54cdcb8114"
     instance_type = "t2.micro"
     availability_zone = "us-west-2"
 }
